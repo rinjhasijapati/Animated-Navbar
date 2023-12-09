f433d6f66eff74f13ee0873da53fb57dbeba036c
rinjhasijapati <aastha_sijapati@yahoo.com>
2023-12-09 20:05:20 +0545

    updated

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,15 @@
+import { useEffect, useState } from "react";
+
+const useLocalStorage = (key, initialValue) => {
+    const [storedValue, setStoredValue] = useState(() => {
+        try {
+            const item = window.localStorage.getItem(key);
+            return item ? JSON.parse(item) : initialValue;
+        } catch (error) {
+            console.log(error);
+            return initialValue;
+        }
+    });
+}
+
+export default useDrakMode;
